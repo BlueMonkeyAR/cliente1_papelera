2720b9c047f105ebc4d07221fe97adcf488ed616
rodrigocbastian <34840898+rodrigocbastian@users.noreply.github.com>
2017-12-27 00:33:00 -0300

    createProduct - v1

---
+++ createProduct.php
@@ -0,0 +1,49 @@
+<?php 	
+
+require_once 'core.php';
+
+$valid['success'] = array('success' => false, 'messages' => array());
+
+if($_POST) {	
+
+	$productName 		= $_POST['productName'];
+  // $productImage 	= $_POST['productImage'];
+  $quantity 			= $_POST['quantity'];
+
+
+  $costo 					= $_POST['costo'];
+  $rate 					= $_POST['rate'];
+  $brandName 			= $_POST['brandName'];
+  $proveedor 			= $_POST['proveedor'];
+  $categoryName 	= $_POST['categoryName'];
+  $productStatus 	= $_POST['productStatus'];
+
+	$type = explode('.', $_FILES['productImage']['name']);
+	$type = $type[count($type)-1];		
+	$url = '../assests/images/stock/'.uniqid(rand()).'.'.$type;
+	if(in_array($type, array('gif', 'jpg', 'jpeg', 'png', 'JPG', 'GIF', 'JPEG', 'PNG'))) {
+		if(is_uploaded_file($_FILES['productImage']['tmp_name'])) {			
+			if(move_uploaded_file($_FILES['productImage']['tmp_name'], $url)) {
+				
+				$sql = "INSERT INTO product (product_name, product_image, brand_id, proveedor, categories_id, quantity, costo, rate, active, status) 
+				VALUES ('$productName', '$url', '$brandName', '$proveedor','$categoryName', '$quantity','$costo' ,'$rate', '$productStatus', 1)";
+
+				if($connect->query($sql) === TRUE) {
+					$valid['success'] = true;
+					$valid['messages'] = "Successfully Added";	
+				} else {
+					$valid['success'] = false;
+					$valid['messages'] = "Error while adding the members";
+				}
+
+			}	else {
+				return false;
+			}	// /else	
+		} // if
+	} // if in_array 		
+
+	$connect->close();
+
+	echo json_encode($valid);
+ 
+} // /if $_POST
